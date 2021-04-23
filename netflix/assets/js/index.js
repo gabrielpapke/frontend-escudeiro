498fc109d187cfef9b7cf6778a0c6e70edfc37fe
Gabriel Papke <gabrielpapke@gmail.com>
2021-04-23 00:54:39 -0300

    netflix: menu, imagem destaque, footer

---
+++ netflix/assets/js/index.js
@@ -0,0 +1,19 @@
+
+
+const scrolledClass = 'scrolled';
+
+window.onscroll = function () {
+    const doc = document.documentElement;
+    const scrollTop = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+    const body = document.body;
+
+    if(scrollTop > 100) {
+        if(!body.classList.contains(scrolledClass)) {
+        return body.classList.add(scrolledClass);
+        }
+    } else {
+        if(body.classList.contains(scrolledClass)) {
+        return body.classList.remove(scrolledClass);
+        }
+    }
+} 
