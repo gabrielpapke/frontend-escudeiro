9577fd88bf1477b8aa1b1ab7c37bfec5be4e6288
Gabriel Papke <gabrielpapke@gmail.com>
2021-04-23 21:57:43 -0300

    netflex: carousel javascript

--- netflix/assets/js/index.js
+++ netflix/assets/js/index.js
@@ -1,7 +1,54 @@
 
+const carousels = document.querySelectorAll('.carousel')
+const thumb_width = 360;
+
+function setWidth (element) {
+    const list = element.querySelector('.carousel__list');
+    const number_items = list.children.length;
+    list.style.width = `${thumb_width * number_items}px`
+}
+
+carousels.forEach(item => {
+    setWidth(item)
+})
+
+function getTranslateX(element) {
+    var style = window.getComputedStyle(element);
+    var matrix = new WebKitCSSMatrix(style.transform);
+
+    return matrix.m41;
+}
+
+function next(event) {
+    const carousel = event.parentNode;
+    const list = carousel.querySelector('.carousel__list');
+    const currentTranslateX = getTranslateX(list)
+    const maxTranslate = Math.abs((list.children.length * thumb_width) - window.outerWidth);
+
+    if (currentTranslateX === -maxTranslate) return
+    
+    let newTranslateX = currentTranslateX - (thumb_width * 2);
+
+    if (newTranslateX < -maxTranslate) newTranslateX = -maxTranslate;
+
+    list.style.transform = "translateX(" + (newTranslateX) + "px)"
+}
+
+function prev(event) {
+    const carousel = event.parentNode;
+    const list = carousel.querySelector('.carousel__list');
+    const currentTranslateX = getTranslateX(list)
+    
+    if (currentTranslateX === 0) return
+
+    let newTranslateX = currentTranslateX + (thumb_width * 2);
+
+    if (newTranslateX > 0) newTranslateX = 0;
+
+    list.style.transform = "translateX(" + (newTranslateX) + "px)"
+}
 
 const scrolledClass = 'scrolled';
-
 window.onscroll = function () {
     const doc = document.documentElement;
     const scrollTop = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
